44a807ac8ac5644d7b3028319c6e33703862b9b9
Anna Cunningham <ac232@hotmail.co.uk>
2021-02-09 18:01:51 +0000

    second park test passing

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -16,7 +16,10 @@ describe('Park', function() {
     assert.strictEqual(actual, 'Jurassic Park');
   });
 
-  it('should have a ticket price');
+  it('should have a ticket price', function(){
+    const actual = park.ticketPrice;
+    assert.strictEqual(actual, 10);
+  });
 
   it('should have a collection of dinosaurs');
 
